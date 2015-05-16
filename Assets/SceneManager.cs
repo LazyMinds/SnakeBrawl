23e931fb065a9d40313d09e484c15ca3a84a720b
Christophe Wang <wchristophe2@gmail.com>
2015-05-16 23:22:23 +0900

    -Implementation d'un menu de base -Suppression du dossier Library inutile sur le dépôt

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	// Changement de Scene
+	public void LoadScene(string sceneName) {
+		Application.LoadLevel (sceneName);
+	}
+}

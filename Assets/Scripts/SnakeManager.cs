4b9b16c36406689e94ef2a12de655a88bc765894
Christophe Wang <wchristophe2@gmail.com>
2015-05-31 18:53:18 +0900

    Refactoring Code Destroy game objects on Game Over

--- Assets/Scripts/SnakeManager.cs
+++ Assets/Scripts/SnakeManager.cs
@@ -5,8 +5,7 @@ using System.Linq;
 
 public class SnakeManager : MonoBehaviour {
 
-	//Prefabs
-	public GameObject snake = null;
+	public GameObject snakeHead = null;
 	public GameObject snakeBody = null;
 	public GameObject snakeTail = null;
 	public GameObject food = null;
@@ -14,201 +13,185 @@ public class SnakeManager : MonoBehaviour {
 	public Sprite spriteHorVerDirection = null;
 	public Sprite spriteAngleDirection = null;
 
-
-	//Border
 	public Transform borderTop = null;
 	public Transform borderBottom = null;
 	public Transform borderLeft = null;
 	public Transform borderRight = null;
 
-	List<GameObject> snakeTailList = new List<GameObject>();
-	private Vector2 m_direction = Vector2.up;
+	private List<GameObject> snakeList = new List<GameObject>();
+	private Vector2 direction = Vector2.up;
 	private bool foodCollision = false;
+    private GameObject bodyObject;
+    private GameObject foodObject;
 
-	enum Direction {GAUCHE, HAUT, BAS, DROITE};
+	enum Direction {LEFT, RIGHT, UP, DOWN};
 
+    /// <summary>
+    /// Initialization
+    /// </summary>
 	void Start () {
-		GameObject tail = Instantiate (snakeTail, snake.transform.position + new Vector3 (0, -32, 0), Quaternion.identity) as GameObject;
-		snakeTailList.Add (tail);
+        snakeList.Add(snakeHead);
+        snakeList.Add(snakeTail);
 		InvokeRepeating ("Move", 0f, 0.15f);
 		Invoke("SpawnFood", 1f);
 	}
 	
-	// Update is called once per frame
+    /// <summary>
+    /// Update for every frame
+    /// </summary>
 	void Update () {
 		if (Input.GetKey ("up")) {
-			if (m_direction != -Vector2.up)
-				m_direction = Vector2.up;
+            if (direction != -Vector2.up)
+                direction = Vector2.up;
 		} else if (Input.GetKey ("down")) {
-			if (m_direction != Vector2.up)
-				m_direction = -Vector2.up;
+            if (direction != Vector2.up)
+                direction = -Vector2.up;
 		} else if (Input.GetKey ("left")) {
-			if (m_direction != Vector2.right)
-				m_direction = -Vector2.right;
+            if (direction != Vector2.right)
+                direction = -Vector2.right;
 		} else if (Input.GetKey ("right")) {
-			if (m_direction != -Vector2.right)
-				m_direction = Vector2.right;
+            if (direction != -Vector2.right)
+                direction = Vector2.right;
 		}
 	}
 
+    /// <summary>
+    /// Object rotation following the direction
+    /// </summary>
+    /// <param name="obj"></param>
+    void Rotate(GameObject obj)
+    {
+        if (direction == Vector2.up)
+            obj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        else if (direction == -Vector2.up)
+            obj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+        else if (direction == Vector2.right)
+            obj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
+        else if (direction == -Vector2.right)
+            obj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+    }
+
+    /// <summary>
+    /// Check the body direction
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    Direction CheckBodyDirection(Vector2 current, Vector2 other)
+    {
+        if (other.x < current.x)
+            return Direction.LEFT;
+        else if (other.x > current.x)
+            return Direction.RIGHT;
+        else if (other.y > current.y)
+            return Direction.UP;
+        else
+            return Direction.DOWN;
+    }
+
+    /// <summary>
+    /// Update the body angle
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="previous"></param>
+    /// <param name="next"></param>
+    void UpdateBodyAngle(GameObject current, Direction previous, Direction next)
+    {
+        if ((previous == Direction.LEFT && next == Direction.RIGHT) || (previous == Direction.RIGHT && next == Direction.LEFT))
+        {
+            current.GetComponent<SpriteRenderer>().sprite = spriteHorVerDirection;
+            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
+        else if ((previous == Direction.UP && next == Direction.DOWN) || (previous == Direction.DOWN && next == Direction.UP))
+        {
+            current.GetComponent<SpriteRenderer>().sprite = spriteHorVerDirection;
+            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+        else if ((previous == Direction.LEFT && next == Direction.DOWN) || (previous == Direction.DOWN && next == Direction.LEFT))
+        {
+            current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
+            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+        }
+        else if ((previous == Direction.LEFT && next == Direction.UP) || (previous == Direction.UP && next == Direction.LEFT))
+        {
+            current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
+            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
+        else if ((previous == Direction.DOWN && next == Direction.RIGHT) || (previous == Direction.RIGHT && next == Direction.DOWN))
+        {
+            current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
+            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
+        }
+        else if ((previous == Direction.RIGHT && next == Direction.UP) || (previous == Direction.UP && next == Direction.RIGHT))
+        {
+            current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
+            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+    }
+
+    /// <summary>
+    /// Snake movements
+    /// </summary>
 	void Move() {
+        Vector2 oldHeadPosition = snakeHead.transform.position;
+        snakeHead.transform.Translate(direction * 32, Space.World);
+		Rotate(snakeHead);
 
-		// Deplacement du snake dans la direction en cour
-		Vector2 oldHeadPosition = snake.transform.position;
-		snake.transform.Translate (m_direction*32, Space.World);
-
-		if (m_direction == Vector2.up)
-			snake.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
-		else if (m_direction == -Vector2.up)
-			snake.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 180));
-		else if (m_direction == Vector2.right)
-			snake.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 270));
-		else if (m_direction == -Vector2.right)
-			snake.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 90));
-
-		Quaternion r = snake.transform.rotation;
-		
 		if (foodCollision) {
-			GameObject body =(GameObject)Instantiate(snakeBody, oldHeadPosition, Quaternion.identity);
-			snakeTailList.Insert(0, body);
+			bodyObject = Instantiate(snakeBody, oldHeadPosition, Quaternion.identity) as GameObject;
+            snakeList.Insert(1, bodyObject);
+            Rotate(snakeList.ElementAt(1));
 			foodCollision = false;
 		}
-		else if (snakeTailList.Count > 0) { // on delete une partie du corp, donc on met la queue a la place de la partie delete
-
-			snakeTailList.Last ().transform.position = oldHeadPosition;
-			snakeTailList.Insert (0, snakeTailList.Last ());
-			snakeTailList.RemoveAt (snakeTailList.Count - 1); 
+        else if (snakeList.Count > 1) {
+            Vector2 previousPosition = oldHeadPosition;
+            Vector2 currentPosition;
+            for (int i = 1; i < snakeList.Count; i++)
+            {
+                currentPosition = snakeList.ElementAt(i).transform.position;
+                snakeList.ElementAt(i).transform.position = previousPosition;
+                previousPosition = currentPosition;
+            }
 		}
 
-		if (snakeTailList.Count > 0) {
-			for (int i = 0; i<snakeTailList.Count; i++)
+        if (snakeList.Count > 1)
+        {
+            for (int i = 1; i < snakeList.Count; i++)
 			{
-				Vector2 beforePosition;
-				if (i == 0)
+                GameObject current = snakeList[i];
+                Direction previous = CheckBodyDirection(current.transform.position, snakeList[i - 1].transform.position);
+                
+                if (i + 1 < snakeList.Count)
+                {
+                    Direction next = CheckBodyDirection(current.transform.position, snakeList[i + 1].transform.position);
+                    UpdateBodyAngle(current, previous, next);
+                }
+                else if (i == snakeList.Count - 1)
 				{
-					beforePosition = snake.transform.position;
-				}
-				else
-				{
-					beforePosition = snakeTailList[i-1].transform.position;
-				}
-				GameObject current = snakeTailList[i];
-
-				Direction before_dir = Direction.HAUT;
-				if (beforePosition.x < current.transform.position.x && beforePosition.y == current.transform.position.y)
-				{
-					before_dir = Direction.GAUCHE;
-				}
-				else if (beforePosition.x > current.transform.position.x && beforePosition.y == current.transform.position.y)
-				{
-					before_dir = Direction.DROITE;
-				}
-				else if (beforePosition.x == current.transform.position.x && beforePosition.y > current.transform.position.y)
-				{
-					before_dir = Direction.HAUT;
-				}
-				else if (beforePosition.x == current.transform.position.x && beforePosition.y < current.transform.position.y)
-				{
-					before_dir = Direction.BAS;
-				}
-
-				if (snakeTailList.Count-1 == i)
-				{
-					if (before_dir == Direction.GAUCHE)
-					{
-						current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 90));
-					}
-					if (before_dir == Direction.DROITE)
-					{
-						current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 270));
-					}
-					if (before_dir == Direction.HAUT)
-					{
-						current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
-					}
-					if (before_dir == Direction.BAS)
-					{
-						current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 170));
-					}
-					current.GetComponent<SpriteRenderer>().sprite = snakeTail.GetComponent<SpriteRenderer>().sprite;
+                    switch (previous)
+                    {
+                        case Direction.LEFT:
+                            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+                            break;
+                        case Direction.RIGHT:
+                            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
+                            break;
+                        case Direction.UP:
+                            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                            break;
+                        case Direction.DOWN:
+                            current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 170));
+                            break;
+                    }
 					return;
 				}
-				Transform after = snakeTailList[i+1].transform;
-
-
-				Direction after_dir = Direction.HAUT;
-				if (after.transform.position.x < current.transform.position.x && after.transform.position.y == current.transform.position.y)
-				{
-					after_dir = Direction.GAUCHE;
-				}
-				else if (after.transform.position.x > current.transform.position.x && after.transform.position.y == current.transform.position.y)
-				{
-					after_dir = Direction.DROITE;
-				}
-				else if (after.transform.position.x == current.transform.position.x && after.transform.position.y > current.transform.position.y)
-				{
-					after_dir = Direction.HAUT;
-				}
-				else if (after.transform.position.x == current.transform.position.x && after.transform.position.y < current.transform.position.y)
-				{
-					after_dir = Direction.BAS;
-				}
-
-				if ((before_dir == Direction.GAUCHE && after_dir == Direction.DROITE) || (before_dir == Direction.DROITE && after_dir == Direction.GAUCHE))
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteHorVerDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 90));
-				}
-				else if ((before_dir == Direction.HAUT && after_dir == Direction.BAS) || (before_dir == Direction.BAS && after_dir == Direction.HAUT))
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteHorVerDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
-				}
-				else if (before_dir == Direction.GAUCHE && after_dir == Direction.BAS)
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 180));
-				}
-				else if (before_dir == Direction.GAUCHE && after_dir == Direction.HAUT)
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 90));
-				}
-				else if (before_dir == Direction.BAS && after_dir == Direction.GAUCHE)
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 180));
-				}
-				else if (before_dir == Direction.BAS && after_dir == Direction.DROITE)
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 270));
-				}
-				else if (before_dir == Direction.DROITE && after_dir == Direction.HAUT)
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
-				}
-				else if (before_dir == Direction.DROITE && after_dir == Direction.BAS)
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 270));
-				}
-				else if (before_dir == Direction.HAUT && after_dir == Direction.GAUCHE)
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 90));
-				}
-				else if (before_dir == Direction.HAUT && after_dir == Direction.DROITE)
-				{
-					current.GetComponent<SpriteRenderer>().sprite = spriteAngleDirection;
-					current.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
-				}
-
 			}
 		}
 	}
 
+    /// <summary>
+    /// Food spawning randomly on the board
+    /// </summary>
 	void SpawnFood() {
 		int x = (int)Random.Range(borderLeft.position.x + 32, borderRight.position.x -32);
 		int y = (int)Random.Range(borderBottom.position.y + 32,borderTop.position.y -32);
@@ -216,9 +199,13 @@ public class SnakeManager : MonoBehaviour {
 		x -= x % 32;
 		y -= y % 32;
 
-		Instantiate(food,new Vector2(x, y),Quaternion.identity);
+		foodObject = Instantiate(food, new Vector2(x, y), Quaternion.identity) as GameObject;
 	}
 
+    /// <summary>
+    /// Collision detection
+    /// </summary>
+    /// <param name="coll"></param>
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.name.StartsWith ("Food")) {
 			foodCollision = true;
@@ -226,11 +213,21 @@ public class SnakeManager : MonoBehaviour {
 			Invoke ("SpawnFood", 0.5f);
 		} else if (coll.name.StartsWith ("BorderTop") || coll.name.StartsWith ("BorderBottom") || 
 		           coll.name.StartsWith ("BorderLeft") || coll.name.StartsWith ("BorderRight")) {
-			Destroy(snake.gameObject);
+            DestroyGameObject();
 			canvas.SetActive(true);
 		} else if (coll.name.StartsWith("SnakeBody") || coll.name.StartsWith("SnakeTail")){
-			Destroy(snake.gameObject);
+            DestroyGameObject();
 			canvas.SetActive(true);
 		}
 	}
+
+    /// <summary>
+    /// Destroy the game object on the board
+    /// </summary>
+    void DestroyGameObject()
+    {
+        for (int i = 0; i < snakeList.Count; i++)
+            Destroy(snakeList[i].gameObject);
+        Destroy(foodObject);
+    }
 }

79c5bc7060412d54a180e8179510a997bcee8307
Christophe Wang <wchristophe2@gmail.com>
2015-05-31 19:07:53 +0900

    Plus de spawn food sur le corps du snake

--- Assets/Scripts/SnakeManager.cs
+++ Assets/Scripts/SnakeManager.cs
@@ -3,57 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class SnakeManager : MonoBehaviour {
+public class SnakeManager : MonoBehaviour
+{
 
-	public GameObject snakeHead = null;
-	public GameObject snakeBody = null;
-	public GameObject snakeTail = null;
-	public GameObject food = null;
-	public GameObject canvas = null;
-	public Sprite spriteHorVerDirection = null;
-	public Sprite spriteAngleDirection = null;
+    public GameObject snakeHead = null;
+    public GameObject snakeBody = null;
+    public GameObject snakeTail = null;
+    public GameObject food = null;
+    public GameObject canvas = null;
+    public Sprite spriteHorVerDirection = null;
+    public Sprite spriteAngleDirection = null;
 
-	public Transform borderTop = null;
-	public Transform borderBottom = null;
-	public Transform borderLeft = null;
-	public Transform borderRight = null;
+    public Transform borderTop = null;
+    public Transform borderBottom = null;
+    public Transform borderLeft = null;
+    public Transform borderRight = null;
 
-	private List<GameObject> snakeList = new List<GameObject>();
-	private Vector2 direction = Vector2.up;
-	private bool foodCollision = false;
+    private List<GameObject> snakeList = new List<GameObject>();
+    private Vector2 direction = Vector2.up;
+    private bool foodCollision = false;
     private GameObject bodyObject;
     private GameObject foodObject;
 
-	enum Direction {LEFT, RIGHT, UP, DOWN};
+    enum Direction { LEFT, RIGHT, UP, DOWN };
 
     /// <summary>
     /// Initialization
     /// </summary>
-	void Start () {
+    void Start()
+    {
         snakeList.Add(snakeHead);
         snakeList.Add(snakeTail);
-		InvokeRepeating ("Move", 0f, 0.15f);
-		Invoke("SpawnFood", 1f);
-	}
-	
+        InvokeRepeating("Move", 0f, 0.15f);
+        Invoke("SpawnFood", 1f);
+    }
+
     /// <summary>
     /// Update for every frame
     /// </summary>
-	void Update () {
-		if (Input.GetKey ("up")) {
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
             if (direction != -Vector2.up)
                 direction = Vector2.up;
-		} else if (Input.GetKey ("down")) {
+        }
+        else if (Input.GetKey("down"))
+        {
             if (direction != Vector2.up)
                 direction = -Vector2.up;
-		} else if (Input.GetKey ("left")) {
+        }
+        else if (Input.GetKey("left"))
+        {
             if (direction != Vector2.right)
                 direction = -Vector2.right;
-		} else if (Input.GetKey ("right")) {
+        }
+        else if (Input.GetKey("right"))
+        {
             if (direction != -Vector2.right)
                 direction = Vector2.right;
-		}
-	}
+        }
+    }
 
     /// <summary>
     /// Object rotation following the direction
@@ -132,18 +142,21 @@ public class SnakeManager : MonoBehaviour {
     /// <summary>
     /// Snake movements
     /// </summary>
-	void Move() {
+    void Move()
+    {
         Vector2 oldHeadPosition = snakeHead.transform.position;
         snakeHead.transform.Translate(direction * 32, Space.World);
-		Rotate(snakeHead);
+        Rotate(snakeHead);
 
-		if (foodCollision) {
-			bodyObject = Instantiate(snakeBody, oldHeadPosition, Quaternion.identity) as GameObject;
+        if (foodCollision)
+        {
+            bodyObject = Instantiate(snakeBody, oldHeadPosition, Quaternion.identity) as GameObject;
             snakeList.Insert(1, bodyObject);
             Rotate(snakeList.ElementAt(1));
-			foodCollision = false;
-		}
-        else if (snakeList.Count > 1) {
+            foodCollision = false;
+        }
+        else if (snakeList.Count > 1)
+        {
             Vector2 previousPosition = oldHeadPosition;
             Vector2 currentPosition;
             for (int i = 1; i < snakeList.Count; i++)
@@ -152,22 +165,22 @@ public class SnakeManager : MonoBehaviour {
                 snakeList.ElementAt(i).transform.position = previousPosition;
                 previousPosition = currentPosition;
             }
-		}
+        }
 
         if (snakeList.Count > 1)
         {
             for (int i = 1; i < snakeList.Count; i++)
-			{
+            {
                 GameObject current = snakeList[i];
                 Direction previous = CheckBodyDirection(current.transform.position, snakeList[i - 1].transform.position);
-                
+
                 if (i + 1 < snakeList.Count)
                 {
                     Direction next = CheckBodyDirection(current.transform.position, snakeList[i + 1].transform.position);
                     UpdateBodyAngle(current, previous, next);
                 }
                 else if (i == snakeList.Count - 1)
-				{
+                {
                     switch (previous)
                     {
                         case Direction.LEFT:
@@ -183,43 +196,66 @@ public class SnakeManager : MonoBehaviour {
                             current.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 170));
                             break;
                     }
-					return;
-				}
-			}
-		}
-	}
+                    return;
+                }
+            }
+        }
+    }
+
+    bool checkCollisionWithBody(int x, int y)
+    {
+        for (int i = 0; i < snakeList.Count; i++)
+        {
+            if (x == snakeList[i].transform.position.x && y == snakeList[i].transform.position.y)
+                return true;
+        }
+        return false;
+    }
 
     /// <summary>
     /// Food spawning randomly on the board
     /// </summary>
-	void SpawnFood() {
-		int x = (int)Random.Range(borderLeft.position.x + 32, borderRight.position.x -32);
-		int y = (int)Random.Range(borderBottom.position.y + 32,borderTop.position.y -32);
+    void SpawnFood()
+    {
+        int x = 0;
+        int y = 0;
 
-		x -= x % 32;
-		y -= y % 32;
+        do
+        {
+            x = (int)Random.Range(borderLeft.position.x + 32, borderRight.position.x - 32);
+            y = (int)Random.Range(borderBottom.position.y + 32, borderTop.position.y - 32);
 
-		foodObject = Instantiate(food, new Vector2(x, y), Quaternion.identity) as GameObject;
-	}
+            x -= x % 32;
+            y -= y % 32;
+        } while (checkCollisionWithBody(x, y));
+
+        foodObject = Instantiate(food, new Vector2(x, y), Quaternion.identity) as GameObject;
+    }
 
     /// <summary>
     /// Collision detection
     /// </summary>
     /// <param name="coll"></param>
-	void OnTriggerEnter2D(Collider2D coll) {
-		if (coll.name.StartsWith ("Food")) {
-			foodCollision = true;
-			Destroy (coll.gameObject);
-			Invoke ("SpawnFood", 0.5f);
-		} else if (coll.name.StartsWith ("BorderTop") || coll.name.StartsWith ("BorderBottom") || 
-		           coll.name.StartsWith ("BorderLeft") || coll.name.StartsWith ("BorderRight")) {
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.name.StartsWith("Food"))
+        {
+            foodCollision = true;
+            Destroy(coll.gameObject);
+            Invoke("SpawnFood", 0.5f);
+        }
+        else if (coll.name.StartsWith("BorderTop") || coll.name.StartsWith("BorderBottom") ||
+                 coll.name.StartsWith("BorderLeft") || coll.name.StartsWith("BorderRight"))
+        {
             DestroyGameObject();
-			canvas.SetActive(true);
-		} else if (coll.name.StartsWith("SnakeBody") || coll.name.StartsWith("SnakeTail")){
+            canvas.SetActive(true);
+        }
+        else if (coll.name.StartsWith("SnakeBody") || coll.name.StartsWith("SnakeTail"))
+        {
             DestroyGameObject();
-			canvas.SetActive(true);
-		}
-	}
+            canvas.SetActive(true);
+        }
+    }
 
     /// <summary>
     /// Destroy the game object on the board
